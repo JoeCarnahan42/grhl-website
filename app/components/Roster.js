dd367ae29409b3400488999044e39772d4a1e52a
JoeCarnahan42 <carnahanjoseph@yahoo.com>
2025-01-31 15:15:42 -0600

    Add more TODO's. Import Roster component to all team pages. Add empty file for roster data.

---
+++ app/components/Roster.js
@@ -0,0 +1,5 @@
+export const Roster = (team) => {
+  // TODO: complete roster function
+  // parameter is a string team name. "Barracudas"
+  return <table className="table table-bordered"></table>;
+};
--- app/teams/barracudas/page.js
+++ app/teams/barracudas/page.js
@@ -1,11 +1,14 @@
 import { Footer } from "@/app/components/Footer";
 import { Header } from "@/app/components/Header";
+import { Roster } from "@/app/components/Roster";
 
 export default function Barracudas() {
   return (
-    <main>
+    <main className="text-center">
       <Header />
       <h1 className="text-center">page content here</h1>
+      <h1>Roster</h1>
+      <Roster props={"Barracudas"} />
       <Footer />
     </main>
   );
