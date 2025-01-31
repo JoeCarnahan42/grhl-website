a59f5b8a0f0fc025d3b38ed15afe5d223751af57
JoeCarnahan42 <carnahanjoseph@yahoo.com>
2025-01-31 14:43:20 -0600

    Add default return for box-scores page

---
+++ app/box-scores/page.js
@@ -0,0 +1,12 @@
+import { Header } from "../components/Header";
+import { Footer } from "../components/Footer";
+
+export default function boxScores() {
+  return (
+    <main>
+      <Header />
+      <h1 className="text-center">page content here</h1>
+      <Footer />
+    </main>
+  );
+}
