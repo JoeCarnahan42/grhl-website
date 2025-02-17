56230e0792c7d8f1d6122bbce0d7eba2384c55c9
Joe <carnahanjoseph@yahoo.com>
2025-02-17 13:14:26 -0600

    begin redux store

--- app/layout.js
+++ app/layout.js
@@ -1,12 +1,8 @@
+'use client'
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./global.css";
 import { Provider } from "react-redux";
 import store from "./store/configureStore";
-
-export const metadata = {
-  title: "Gateway Roller Hockey League",
-  description: "Recreation Roller Hockey League",
-};
 
 export default function RootLayout({ children }) {
   return (
