8f81b292949928642affb39f9712c9dd1499276b
Joe <carnahanjoseph@yahoo.com>
2025-02-17 12:00:05 -0600

    Intall redux, Add TODO's

---
+++ app/components/PlayerProfile.js
@@ -0,0 +1,1 @@
+// TODO: Complete individual player profile component
--- app/components/TeamPageHeader.js
+++ app/components/TeamPageHeader.js
@@ -2,10 +2,15 @@
 // Roster/Cap outlook/Schedule
 
 export const TeamPageHeader = () => {
+  
   return (
     <div className="container text-center">
-      <nav>
-        <h1>Nav text here</h1>
+      <nav className="navbar justify-content-center nav-text">
+        <ul style={{ border: "none" }} className="list-group list-group-horizontal">
+          <li style={{ border: "none" }} className="list-group-item mx-5"><button className="btn btn-light"><u>Roster</u></button></li>
+          <li style={{ border: "none" }} className="list-group-item mx-5"><button className="btn btn-light"><u>Cap Outlook</u></button></li>
+          <li style={{ border: "none" }} className="list-group-item mx-5"><button className="btn btn-light"><u>Transactions</u></button></li>
+        </ul>
       </nav>
     </div>
   )
