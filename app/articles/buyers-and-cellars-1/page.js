b4b581afec7604424d6904214ad2e184f9e22f24
JoeCarnahan42 <carnahanjoseph@yahoo.com>
2025-02-19 14:09:41 -0600

    Start articles/Add TODO's

---
+++ app/articles/buyers-and-cellars-1/page.js
@@ -0,0 +1,13 @@
+import { Header } from "/app/components/Header";
+import { Footer } from "/app/components/Footer";
+
+export default function buyersAndCellarsOne() {
+  return (
+    <main className="text-center">
+      <Header />
+      {/*TODO: Add back button */}
+      <h1>Buyers and Cellars</h1>
+      <Footer />
+    </main>
+  );
+}
---
+++ app/articles/buyers-and-cellars-2/page.js
@@ -0,0 +1,12 @@
+import { Header } from "/app/components/Header";
+import { Footer } from "/app/components/Footer";
+export default function buyersAndCellarsTwo() {
+  return (
+    <main className="text-center">
+      <Header />
+      {/*TODO: Add back button */}
+      <h1>Buyers and Cellars pt.2</h1>
+      <Footer />
+    </main>
+  );
+}
