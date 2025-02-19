fdc7fc88e1060b0c99a7c653de9301830ffdd41e
JoeCarnahan42 <carnahanjoseph@yahoo.com>
2025-02-19 12:43:02 -0600

    Fix typo

--- app/show-me-division/page.js
+++ app/show-me-division/page.js
@@ -5,28 +5,39 @@ import Image from "next/image";
 import Link from "next/link";
 
 export default function LincolnDiv() {
-  const lincolnDiv = records[1]
-
+  const lincolnDiv = records[1];
 
   return (
     <main className="text-center">
       <Header />
-      <Image width={267} height={327} alt="Division Logo" src={'/media/images/division-images/grhl-showme-div-logo.avif'} />
+      <Image
+        width={267}
+        height={327}
+        alt="Division Logo"
+        src={"/media/images/division-images/grhl-showme-div-logo.avif"}
+      />
       <br />
       <br />
-      <h1 className="text-center"><u>Lincoln Division Teams</u></h1>
+      <h1 className="text-center">
+        <u>Show-Me Division Teams</u>
+      </h1>
       <br />
       <div className="container">
         <div className="row">
           {lincolnDiv.map((team) => {
             return (
-              <div className="col text-center">
+              <div key={team.name} className="col text-center">
                 <Link href={`/teams/${team.name.toLowerCase()}`}>
-                  <Image width={200} height={200} alt={`${team.name} Logo`} src={team.img} />
+                  <Image
+                    width={200}
+                    height={200}
+                    alt={`${team.name} Logo`}
+                    src={team.img}
+                  />
                   <h2>{team.name}</h2>
                 </Link>
               </div>
-            )
+            );
           })}
         </div>
       </div>
