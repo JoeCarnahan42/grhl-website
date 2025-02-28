dee0af5251c14fbffeb52e15fe3f5c2dc1aea609
JoeCarnahan42 <carnahanjoseph@yahoo.com>
2025-02-28 13:05:34 -0600

    Complete waiver page

--- app/waivers/page.js
+++ app/waivers/page.js
@@ -1,13 +1,17 @@
-import { Header } from "../components/Header"
-import { Footer } from "../components/Footer"
-
+import { Header } from "../components/Header";
+import { Footer } from "../components/Footer";
+import { Roster } from "../components/Roster";
+import { TeamPageHeader } from "../components/TeamPageHeader";
 
 export default function Waivers() {
   return (
     <main>
       <Header />
-      <h1 className="text-center">page content here</h1>
+      <TeamPageHeader />
+      <br />
+      <h1 className="text-center">2024-2025 Waivers</h1>
+      <Roster team="waivers" />
       <Footer />
     </main>
-  )
+  );
 }
