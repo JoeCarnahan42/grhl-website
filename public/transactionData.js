910c253ad93dba700eb8103a2d76b157988a5766
Joe Carnahan <carnahanjoseph@yahoo.com>
2025-04-18 07:00:59 -0500

    start team info panels

---
+++ public/transactionData.js
@@ -0,0 +1,3 @@
+export const transactionData = {
+    wolves: [{date: "12/03/24", trade: "Wolves acquire Kyle Gehrig and Bryan Rucker for Josh Heatherly, Chris Niemeyer, and $2 in Trade Cap."}, {date: "08/11/24", trade: "Wolves receive 1 Cap Year from the Barracudas in exchange for $1 in Cap (for '24-'25)."}]
+}
